21a97a30ccf18dcc6bba079ae344ad93b7e5ad45
Gantsooj <Gantsoojkaka@gmail.com>
2016-11-16 17:09:36 +0800

    footer version 1

--- resources/views/partial/footer.blade.php
+++ resources/views/partial/footer.blade.php
@@ -1,3 +1,42 @@
 <div class="container">
-footer
+    <section style="height:80px;"></section>
+
+    <!----------- Footer ------------>
+    <footer class="footer-bs">
+        <div class="row">
+        	<div class="col-md-3 footer-brand animated fadeInLeft">
+            	<h2>Зураг</h2>
+                <p> Байгууллагын танилцуулга</p>
+                <h4>Хаяг</h4>
+                <p> Орхон аймаг, Баян-Өндөр сум, Вэйс компанийн байр</p>
+                <h4>Утас</h4>
+                <p> 99999999, 99999999, 99999999</p>
+                <h4>Емайл</h4>
+                <p> info@bella.mn</p>
+                <p>© 2016 Bella Salon, All rights reserved</p>
+            </div>
+        	<div class="col-md-4 footer-nav animated fadeInUp">
+            	
+            </div>
+        	<div class="col-md-3 footer-ns animated fadeInRight">
+            	<h4>Social</h4>
+            	<ul>
+                	<li><a href="#">Facebook</a></li>
+                	<li><a href="#">Twitter</a></li>
+                	<li><a href="#">Instagram</a></li>
+                </ul>
+                
+            </div>
+            <div class="col-md-2 footer-social animated fadeInDown">
+            	<h4>Social</h4>
+            	<ul>
+                	<li><a href="#">Facebook</a></li>
+                	<li><a href="#">Twitter</a></li>
+                	<li><a href="#">Instagram</a></li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+    <section class="footer-sec"><p>Designed by <a href="http://enfoplus.net">Tagtaa Solution Developer Team</a></p></section>
+
 </div>
