e585ce4384cc2f2cb4e16ca99b1f78d75e7aea45
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2016-11-23 21:41:14 +0800

    admin routes

--- routes/web/admin.php
+++ routes/web/admin.php
@@ -1,4 +1,24 @@
 <?php
 Route::get('dashboard', function () {
-    return view('dashboard');
+    return view('admin/dashboard');
+});
+
+Route::get('timeline', function () {
+    return view('admin/timeline');
+});
+
+Route::get('index', function () {
+    return view('admin/index');
+});
+
+Route::get('index2', function () {
+    return view('admin/index2');
+});
+
+Route::get('widgets', function () {
+    return view('admin/widgets');
+});
+
+Route::get('general', function () {
+    return view('admin/general');
 });
