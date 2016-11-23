a92771ea7b5ed2ddb3355914dfd0f0ddf627120f
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2016-11-23 19:39:56 +0800

    барааны дэлгэрэнгүй зураг зассан

--- resources/views/products/detailProd.blade.php
+++ resources/views/products/detailProd.blade.php
@@ -92,7 +92,7 @@
 					@foreach($product->features['images'] as $image)
 						<li>
 							<a class="thumbnail">
-								<img src = "img/no-image.jpg?h=60" lazy-img = "{{ $image }}?h=60" class="img-responsive img-rounded" ng-click = "setPortrait('{{ $image }}?w=450')">
+								<img src = "img/no-image.jpg?h=60" lazy-img = "{{ $image }}?h=60" class="pd-image img-responsive img-rounded" ng-click = "setPortrait('{{ $image }}?w=450')">
 							</a>
 						</li>
 						<?php $gallery .= $image.'?w=450,'; ?>
