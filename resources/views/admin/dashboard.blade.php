ef4a23129959c8a717cade4594cc0f4f096c4911
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2016-11-23 21:41:05 +0800

    dashboard blades added

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,7 +4,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>ReactJS-AdminLTE | Dashboard</title>
+        <title>Удирдлагын хэсэг</title>
         <!-- Tell the browser to be responsive to screen width -->
         <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
         name="viewport">
