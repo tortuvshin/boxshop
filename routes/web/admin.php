d3946f5c6d3dbb88396e52c950f9772a42a8d72e
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2016-11-24 14:27:31 +0800

    admin хэсэгрүү зөвхөн админ хандах эрхтэй болгосон

--- routes/web/admin.php
+++ routes/web/admin.php
@@ -1,7 +1,15 @@
 <?php
-Route::get('admin/dashboard', ['as' => '', 'uses' => 'AdminController@dashboard']);
-Route::get('admin/timeline', ['as' => 'admin/timeline', 'uses' => 'AdminController@timeline']);
-Route::get('admin/index', ['as' => 'admin/index', 'uses' => 'AdminController@index']);
-Route::get('admin/widgets', ['as' => 'admin/widgets', 'uses' => 'AdminController@widgets']);
-Route::get('admin/general', ['as' => 'admin/general', 'uses' => 'AdminController@general']);
 
+Route::group(['prefix' => 'admin', 'roles' => 'admin', 'middleware' => ['auth', 'roles']], function () {
+   	
+   	Route::get('dashboard', ['as' => '', 'uses' => 'AdminController@dashboard']);
+	
+	Route::get('timeline', ['as' => 'admin/timeline', 'uses' => 'AdminController@timeline']);
+	
+	Route::get('index', ['as' => 'admin/index', 'uses' => 'AdminController@index']);
+	
+	Route::get('widgets', ['as' => 'admin/widgets', 'uses' => 'AdminController@widgets']);
+	
+	Route::get('general', ['as' => 'admin/general', 'uses' => 'AdminController@general']);
+
+});
