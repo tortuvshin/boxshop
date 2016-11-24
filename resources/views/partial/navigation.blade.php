9cbe372f0fbfc7fae664525d7fdbf02b57ce6ccb
user-PC\user <dambiidark@gmail.com>
2016-11-24 13:38:59 +0800

    ?

--- resources/views/partial/navigation.blade.php
+++ resources/views/partial/navigation.blade.php
@@ -56,7 +56,7 @@
 						
 				<li class="dropdown">
 					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-						<span class="fui fui-heart"></span>
+						<span class="glyphicon glyphicon-heart"></span>
 					</a>
 					<ul class="dropdown-menu" role="menu">
 						<li><a href="{{ route('orders.show_wish_list') }}">{{ trans('store.wish_list') }}</a></li>
