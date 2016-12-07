93d54a1cad341f0994bfb73279e726d5fd03f004
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2016-12-07 23:24:11 +0800

    navigation bottom added

---
+++ resources/views/partial/navigation_bottom.blade.php
@@ -0,0 +1,86 @@
+<div class="header-bottom-content">
+    <div class="">
+        <div class="row" style="position: relative">
+            <div class="col-md-2 col-sm-2 col-xs-12">
+                <div class="row">
+                    <h3 class="full-category" id="full-category"><i class="fa fa-navicon"></i>Бүх ангилал</h3>
+                </div>
+            </div>
+            <div class="col-md-10 col-sm-10 col-xs-12">
+                <div class="row">
+                    <ul class="line-menu">
+                        <li><a class="kk-1" href="http://mimico.mn/featured/product">Онцлох бараа</a></li>
+                        <li><a class="kk-1" href="http://mimico.mn/new/product">Шинэ</a></li>
+                        <li><a class="kk-1" href="http://mimico.mn/best/product">Шилдэг</a></li>
+                        <li><a class="kk-1" href="http://mimico.mn/brand">Брэнд</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div id="full-category-box" class="full-category-box hidden">
+                <div class="col-md-3 col-sm-4 col-xs-12">
+                    <div class="row">
+                        <h3><a href="http://mimico.mn/category/ars-archilgaa">Арьс арчилгаа</a></h3>
+                        <ul class="full-category-child list-unstyled">
+
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-4 col-xs-12">
+                    <div class="row">
+                        <h3><a href="http://mimico.mn/category/nuur-budalt">Нүүр будалт</a></h3>
+                        <ul class="full-category-child list-unstyled">
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-4 col-xs-12">
+                    <div class="row">
+                        <h3><a href="http://mimico.mn/category/us-bie-archilgaa">Үс / бие арчилгаа</a></h3>
+                        <ul class="full-category-child list-unstyled">
+
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-4 col-xs-12">
+                    <div class="row">
+                        <h3><a href="http://mimico.mn/category/unertei-us">Үнэртэй ус</a></h3>
+                        <ul class="full-category-child list-unstyled">
+                        </ul>
+                    </div>
+                </div>
+                <div class="clr"></div>
+                <div class="col-md-3 col-sm-4 col-xs-12">
+                    <div class="row">
+                        <h3><a href="http://mimico.mn/category/bagc">Багц</a></h3>
+                        <ul class="full-category-child list-unstyled">
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-4 col-xs-12">
+                    <div class="row">
+                        <h3><a href="http://mimico.mn/category/eruul-mend">Эрүүл мэнд</a></h3>
+                        <ul class="full-category-child list-unstyled">
+
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-4 col-xs-12">
+                    <div class="row">
+                        <h3><a href="http://mimico.mn/category/dijital-baraa">Дижитал бараа</a></h3>
+                        <ul class="full-category-child list-unstyled">
+
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-4 col-xs-12">
+                    <div class="row">
+                        <h3><a href="http://mimico.mn/category/hyamdraltai-baraa">Хямдралтай бараа</a></h3>
+                        <ul class="full-category-child list-unstyled">
+
+                        </ul>
+                    </div>
+                </div>
+                <div class="clr"></div>
+            </div>
+        </div>
+    </div>
+</div>
