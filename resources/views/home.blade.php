28299adcd4e735a801bee715d10e1b6066cbb889
user-PC\user <dambiidark@gmail.com>
2016-12-08 17:39:54 +0800

    home angilal section nemsen

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -71,6 +71,8 @@
 
             <div class="col-md-10 home-carousel-box">
 
+
+
                 <div id="store-home-carousel" class="carousel slide" data-ride="carousel">
 
                     {{-- indicators --}}
@@ -227,5 +229,445 @@
         </div> {{-- end container-fluid --}}
 
     </section> {{-- end products_view --}}
+
+      <!-- product section start-->
+              <section>
+                <div class="container" style="padding-left: 0px;
+    padding-right: 0px;
+    box-shadow: 0 3px 6px 0 rgba(51, 51, 51, 0.3);">
+                      
+                    <div class="col-lg-2" style="       padding-right: 0;
+    padding-left: 0px;">
+                        <div class="" style="background-color: #3face3;">
+                            <p style="text-transform: uppercase;
+    font-size: 25px;
+    line-height: 30px;
+    color: #fff;
+    margin-bottom: 0;
+    padding: 15px 20px;
+    word-wrap: break-word;">COMPUTER & ELECTRONICS</p>
+                        </div>
+                        <div class="row demo" style="background: url(http://demo2.wpdance.com/imgs/wd_glory/supermarket2/supermarket2_product_group5_head.png) bottom left no-repeat;">
+                        <ul style=" padding-top: 14px;
+    padding-left: 0px;
+    margin-bottom: 0;
+    padding-bottom: 220px;
+    border-right: 1px solid #ebebeb;">
+
+                        <li style="border-color: #ebebeb; ">Tops</li>
+
+                            <li>
+                                <a href="http://mimico.mn/category/ars-archilgaa">Арьс арчилгаа</a>
+                                
+                            </li>
+                            <li>
+                                <a href="http://mimico.mn/category/nuur-budalt">Нүүр будалт</a>
+                                
+                            </li>
+                            <li>
+                                <a href="http://mimico.mn/category/us-bie-archilgaa">Үс / бие арчилгаа</a>
+                                
+                            </li>
+                            <li>
+                                <a href="http://mimico.mn/category/unertei-us">Үнэртэй ус</a>
+                                
+                            </li>
+                            <li>
+                                <a href="http://mimico.mn/category/bagc">Багц</a>
+                                
+                            </li>
+                            <li>
+                                <a href="http://mimico.mn/category/eruul-mend">Эрүүл мэнд</a>
+                                
+                            </li>
+                            <li>
+                                <a href="http://mimico.mn/category/dijital-baraa">Дижитал бараа</a>
+                                
+                            </li>
+                            <li>
+                                <a href="http://mimico.mn/category/hyamdraltai-baraa">Хямдралтай бараа</a>
+                                
+                            </li>
+                        </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-10" style="border-top: 2px solid #3face3; padding: 20px">
+
+                    <div class="col-lg-5" ><img alt="Big title goes here" title="Big title goes here" style="    width: 100%;" src="http://demo2.wpdance.com/imgs/wd_glory/supermarket2/banner/supermarket2_product_group7_content.jpg"></div>
+                    <div class="col-lg-7" style="padding-left: 0px;
+    padding-right: 0px;">
+                        <div class="col-lg-4 clearfix product-overflow">
+
+    
+    <div class="product-box clearfix product-overflow ng-scope" ng-controller="ProductBox">
+
+        <div class="product-reviews  hide ">
+            
+            <small>
+                Харах
+            </small>
+        </div>
+
+        
+        <div class="product-img-box" ng-click="goTo('http://localhost/products/30')">
+
+                <span class="product-image-hover"></span>       
+                            <img src="/img/pt-default/6.jpg?h=250" alt="PINK TONE UP">
+                    </div>
+<!-- 
+        <p class="product-description"></p>
+ -->
+        
+        <div class="product-actions actions">
+
+            
+            <div class="col-lg-6 col-md-4 col-sm-4 col-xs-4 wrapper add_to_card" ng-click="submit('#add-30')">
+                <div class="glyphicon glyphicon-shopping-cart option ">
+                                            <form method="POST" action="http://localhost/user/orders/addTo/cart/30" accept-charset="UTF-8" id="add-30" class="ng-pristine ng-valid"><input name="_method" type="hidden" value="PUT" autocomplete="off"><input name="_token" type="hidden" value="NT7ohrAq7xknoLgYlANFpBgMvWhx7FCOGwfwLa53" autocomplete="off">
+                        </form>
+                                    </div>
+                <span id="card_text">Сагс</span>
+            </div>
+
+            
+            <div class="col-lg-3 col-md-4 col-sm-4 col-xs-4 wrapper add_to_heart" ng-click="goTo('http://localhost/user/orders/addTo/wishlist/30')">
+                <div class="glyphicon glyphicon-heart option"></div>
+            </div>
+
+            
+            <div class="col-lg-3 col-md-4 col-sm-4 col-xs-4 wrapper quick_view" ng-click="goTo('http://localhost/products/30')">
+                <div class="glyphicon glyphicon-eye-open option"></div>
+            </div>
+            
+
+        </div>
+        
+        <h6 class="product-name">
+            <a href="http://localhost/products/30">
+                PINK TONE UP
+            </a>
+        </h6>
+
+        <div class="product-price">
+            USD95.00
+        </div>
+
+    </div>
+    
+
+
+</div>
+<div class="col-lg-4 clearfix product-overflow">
+
+    
+    <div class="product-box clearfix product-overflow ng-scope" ng-controller="ProductBox">
+
+        <div class="product-reviews  hide ">
+            
+            <small>
+                Харах
+            </small>
+        </div>
+
+        
+        <div class="product-img-box" ng-click="goTo('http://localhost/products/30')">
+
+                <span class="product-image-hover"></span>       
+                            <img src="/img/pt-default/6.jpg?h=250" alt="PINK TONE UP">
+                    </div>
+<!-- 
+        <p class="product-description"></p>
+ -->
+        
+        <div class="product-actions actions">
+
+            
+            <div class="col-lg-6 col-md-4 col-sm-4 col-xs-4 wrapper add_to_card" ng-click="submit('#add-30')">
+                <div class="glyphicon glyphicon-shopping-cart option ">
+                                            <form method="POST" action="http://localhost/user/orders/addTo/cart/30" accept-charset="UTF-8" id="add-30" class="ng-pristine ng-valid"><input name="_method" type="hidden" value="PUT" autocomplete="off"><input name="_token" type="hidden" value="NT7ohrAq7xknoLgYlANFpBgMvWhx7FCOGwfwLa53" autocomplete="off">
+                        </form>
+                                    </div>
+                <span id="card_text">Сагс</span>
+            </div>
+
+            
+            <div class="col-lg-3 col-md-4 col-sm-4 col-xs-4 wrapper add_to_heart" ng-click="goTo('http://localhost/user/orders/addTo/wishlist/30')">
+                <div class="glyphicon glyphicon-heart option"></div>
+            </div>
+
+            
+            <div class="col-lg-3 col-md-4 col-sm-4 col-xs-4 wrapper quick_view" ng-click="goTo('http://localhost/products/30')">
+                <div class="glyphicon glyphicon-eye-open option"></div>
+            </div>
+            
+
+        </div>
+        
+        <h6 class="product-name">
+            <a href="http://localhost/products/30">
+                PINK TONE UP
+            </a>
+        </h6>
+
+        <div class="product-price">
+            USD95.00
+        </div>
+
+    </div>
+    
+
+
+</div>
+<div class="col-lg-4 clearfix product-overflow">
+
+    
+    <div class="product-box clearfix product-overflow ng-scope" ng-controller="ProductBox">
+
+        <div class="product-reviews  hide ">
+            
+            <small>
+                Харах
+            </small>
+        </div>
+
+        
+        <div class="product-img-box" ng-click="goTo('http://localhost/products/30')">
+
+                <span class="product-image-hover"></span>       
+                            <img src="/img/pt-default/6.jpg?h=250" alt="PINK TONE UP">
+                    </div>
+<!-- 
+        <p class="product-description"></p>
+ -->
+        
+        <div class="product-actions actions">
+
+            
+            <div class="col-lg-6 col-md-4 col-sm-4 col-xs-4 wrapper add_to_card" ng-click="submit('#add-30')">
+                <div class="glyphicon glyphicon-shopping-cart option ">
+                                            <form method="POST" action="http://localhost/user/orders/addTo/cart/30" accept-charset="UTF-8" id="add-30" class="ng-pristine ng-valid"><input name="_method" type="hidden" value="PUT" autocomplete="off"><input name="_token" type="hidden" value="NT7ohrAq7xknoLgYlANFpBgMvWhx7FCOGwfwLa53" autocomplete="off">
+                        </form>
+                                    </div>
+                <span id="card_text">Сагс</span>
+            </div>
+
+            
+            <div class="col-lg-3 col-md-4 col-sm-4 col-xs-4 wrapper add_to_heart" ng-click="goTo('http://localhost/user/orders/addTo/wishlist/30')">
+                <div class="glyphicon glyphicon-heart option"></div>
+            </div>
+
+            
+            <div class="col-lg-3 col-md-4 col-sm-4 col-xs-4 wrapper quick_view" ng-click="goTo('http://localhost/products/30')">
+                <div class="glyphicon glyphicon-eye-open option"></div>
+            </div>
+            
+
+        </div>
+        
+        <h6 class="product-name">
+            <a href="http://localhost/products/30">
+                PINK TONE UP
+            </a>
+        </h6>
+
+        <div class="product-price">
+            USD95.00
+        </div>
+
+    </div>
+    
+
+
+</div>
+<div class="col-lg-4 clearfix product-overflow">
+
+    
+    <div class="product-box clearfix product-overflow ng-scope" ng-controller="ProductBox">
+
+        <div class="product-reviews  hide ">
+            
+            <small>
+                Харах
+            </small>
+        </div>
+
+        
+        <div class="product-img-box" ng-click="goTo('http://localhost/products/30')">
+
+                <span class="product-image-hover"></span>       
+                            <img src="/img/pt-default/6.jpg?h=250" alt="PINK TONE UP">
+                    </div>
+<!-- 
+        <p class="product-description"></p>
+ -->
+        
+        <div class="product-actions actions">
+
+            
+            <div class="col-lg-6 col-md-4 col-sm-4 col-xs-4 wrapper add_to_card" ng-click="submit('#add-30')">
+                <div class="glyphicon glyphicon-shopping-cart option ">
+                                            <form method="POST" action="http://localhost/user/orders/addTo/cart/30" accept-charset="UTF-8" id="add-30" class="ng-pristine ng-valid"><input name="_method" type="hidden" value="PUT" autocomplete="off"><input name="_token" type="hidden" value="NT7ohrAq7xknoLgYlANFpBgMvWhx7FCOGwfwLa53" autocomplete="off">
+                        </form>
+                                    </div>
+                <span id="card_text">Сагс</span>
+            </div>
+
+            
+            <div class="col-lg-3 col-md-4 col-sm-4 col-xs-4 wrapper add_to_heart" ng-click="goTo('http://localhost/user/orders/addTo/wishlist/30')">
+                <div class="glyphicon glyphicon-heart option"></div>
+            </div>
+
+            
+            <div class="col-lg-3 col-md-4 col-sm-4 col-xs-4 wrapper quick_view" ng-click="goTo('http://localhost/products/30')">
+                <div class="glyphicon glyphicon-eye-open option"></div>
+            </div>
+            
+
+        </div>
+        
+        <h6 class="product-name">
+            <a href="http://localhost/products/30">
+                PINK TONE UP
+            </a>
+        </h6>
+
+        <div class="product-price">
+            USD95.00
+        </div>
+
+    </div>
+    
+
+
+</div>
+<div class="col-lg-4 clearfix product-overflow">
+
+    
+    <div class="product-box clearfix product-overflow ng-scope" ng-controller="ProductBox">
+
+        <div class="product-reviews  hide ">
+            
+            <small>
+                Харах
+            </small>
+        </div>
+
+        
+        <div class="product-img-box" ng-click="goTo('http://localhost/products/30')">
+
+                <span class="product-image-hover"></span>       
+                            <img src="/img/pt-default/6.jpg?h=250" alt="PINK TONE UP">
+                    </div>
+<!-- 
+        <p class="product-description"></p>
+ -->
+        
+        <div class="product-actions actions">
+
+            
+            <div class="col-lg-6 col-md-4 col-sm-4 col-xs-4 wrapper add_to_card" ng-click="submit('#add-30')">
+                <div class="glyphicon glyphicon-shopping-cart option ">
+                                            <form method="POST" action="http://localhost/user/orders/addTo/cart/30" accept-charset="UTF-8" id="add-30" class="ng-pristine ng-valid"><input name="_method" type="hidden" value="PUT" autocomplete="off"><input name="_token" type="hidden" value="NT7ohrAq7xknoLgYlANFpBgMvWhx7FCOGwfwLa53" autocomplete="off">
+                        </form>
+                                    </div>
+                <span id="card_text">Сагс</span>
+            </div>
+
+            
+            <div class="col-lg-3 col-md-4 col-sm-4 col-xs-4 wrapper add_to_heart" ng-click="goTo('http://localhost/user/orders/addTo/wishlist/30')">
+                <div class="glyphicon glyphicon-heart option"></div>
+            </div>
+
+            
+            <div class="col-lg-3 col-md-4 col-sm-4 col-xs-4 wrapper quick_view" ng-click="goTo('http://localhost/products/30')">
+                <div class="glyphicon glyphicon-eye-open option"></div>
+            </div>
+            
+
+        </div>
+        
+        <h6 class="product-name">
+            <a href="http://localhost/products/30">
+                PINK TONE UP
+            </a>
+        </h6>
+
+        <div class="product-price">
+            USD95.00
+        </div>
+
+    </div>
+    
+
+
+</div>
+<div class="col-lg-4 clearfix product-overflow">
+
+    
+    <div class="product-box clearfix product-overflow ng-scope" ng-controller="ProductBox">
+
+        <div class="product-reviews  hide ">
+            
+            <small>
+                Харах
+            </small>
+        </div>
+
+        
+        <div class="product-img-box" ng-click="goTo('http://localhost/products/30')">
+
+                <span class="product-image-hover"></span>       
+                            <img src="/img/pt-default/6.jpg?h=250" alt="PINK TONE UP">
+                    </div>
+<!-- 
+        <p class="product-description"></p>
+ -->
+        
+        <div class="product-actions actions">
+
+            
+            <div class="col-lg-6 col-md-4 col-sm-4 col-xs-4 wrapper add_to_card" ng-click="submit('#add-30')">
+                <div class="glyphicon glyphicon-shopping-cart option ">
+                                            <form method="POST" action="http://localhost/user/orders/addTo/cart/30" accept-charset="UTF-8" id="add-30" class="ng-pristine ng-valid"><input name="_method" type="hidden" value="PUT" autocomplete="off"><input name="_token" type="hidden" value="NT7ohrAq7xknoLgYlANFpBgMvWhx7FCOGwfwLa53" autocomplete="off">
+                        </form>
+                                    </div>
+                <span id="card_text">Сагс</span>
+            </div>
+
+            
+            <div class="col-lg-3 col-md-4 col-sm-4 col-xs-4 wrapper add_to_heart" ng-click="goTo('http://localhost/user/orders/addTo/wishlist/30')">
+                <div class="glyphicon glyphicon-heart option"></div>
+            </div>
+
+            
+            <div class="col-lg-3 col-md-4 col-sm-4 col-xs-4 wrapper quick_view" ng-click="goTo('http://localhost/products/30')">
+                <div class="glyphicon glyphicon-eye-open option"></div>
+            </div>
+            
+
+        </div>
+        
+        <h6 class="product-name">
+            <a href="http://localhost/products/30">
+                PINK TONE UP
+            </a>
+        </h6>
+
+        <div class="product-price">
+            USD95.00
+        </div>
+
+    </div>
+    
+
+
+</div>
+
+                    </div>
+                    </div>
+                
+                </div>
+              </section>
+                <!-- product section end-->
 
 @stop {{-- end content --}}
