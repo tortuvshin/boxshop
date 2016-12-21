7aabb05437062e0815ab53ee8de06474123fa4fc
doljko <doljko_tv@yahoo.com>
2016-12-21 18:31:47 +0800

    slider зассан

--- resources/views/taniltsulga/index.blade.php
+++ resources/views/taniltsulga/index.blade.php
@@ -11,7 +11,7 @@
 
 
         <!-- Wrapper for slides -->
-        <div class="carousel-inner" role="listbox">
+        <div class="carousel-inner bella-slider" role="listbox">
 
             <div class="item active">
                 <img src="http://html.crunchpress.com/big-salon/images/banner/banner-img-03.jpg">
