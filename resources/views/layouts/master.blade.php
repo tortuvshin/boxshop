1a2d9af7db92e85447d87527635f2dab19b8ace2
doljko <doljko_tv@yahoo.com>
2016-12-21 18:03:52 +0800

    танилцуулга хуудасны загвар master

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,6 +6,9 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<base href="/">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 		<meta name="description" content="">
 		<meta name="author" content="">
 	@show
