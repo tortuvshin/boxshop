acbae8afc8c67b67ba5e540a2c4f7b31674c415e
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2016-12-24 15:56:47 +0800

    Цаг авах Model Controller бичсэн #48

--- app/Http/Controllers/TimeController.php
+++ app/Http/Controllers/TimeController.php
@@ -11,4 +11,53 @@ class TimeController extends Controller
    	public function index () {
    		return view('time/index');
    	}
+
+
+   	protected function timeAdded(Request $request)
+    {
+        $this->validate($request, $this->rules());
+
+        $user = $this->createTime($request->all());
+
+        return redirect($this->index());
+    }
+
+    /**
+     * Цаг бүртгэлд шаардлагатай мэдээлэл шалгах
+     *
+     * @return array
+     */
+    protected function rules()
+    {
+        return [
+            'sector' 	   => 'required',
+	        'service' 	   => 'required',
+	        'service_date' => 'required',
+	        'userName' 	   => 'required|min:6',
+	        'userNumber'   => 'required',
+	        'addition'     => 'required',
+	        'description'  => 'required',
+        ];
+    }
+
+    /**
+     * цаг бүртгэл.
+     *
+     * @param  array  $data
+     * @return Time
+     */
+    protected function createTime(array $data)
+    {
+        $time = Time::create([
+            'sector'       => $data['sector'],
+            'service'      => $data['service'],
+            'service_date' => $data['service_date'],
+            'userName'     => $data['person'],
+            'userNumber'   => $data['userNumber'],
+            'addition'     => $data['addition'],
+            'description'  => $data['description'],
+        ]);
+
+        return $time;
+    }
 }
--- app/Time.php
+++ app/Time.php
@@ -7,4 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class Time extends Model
 {
     //
+
+    protected $table = 'userTime';
+
+
+    protected $fillable = [
+        'sector',
+        'service',
+        'service_date',
+        'userName',
+        'userNumber',
+        'addition',
+        'description',
+    ];
 }
