5817c6391af9d9f8f99c745a45348b42c9defe23
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2016-12-26 12:43:44 +0800

    танилцуулга слайдер устгасан

--- resources/views/taniltsulga/index.blade.php
+++ resources/views/taniltsulga/index.blade.php
@@ -7,57 +7,6 @@
 @section('content')
 
 
-    <div id="myCarousel" class="carousel slide">
-
-
-        <!-- Wrapper for slides -->
-        <div class="carousel-inner bella-slider" role="listbox">
-
-            <div class="item active">
-                <img src="http://html.crunchpress.com/big-salon/images/banner/banner-img-03.jpg">
-                <div class="carousel-caption">
-                    <h3>Chania</h3>
-                    <p>The atmosphere in Chania has a touch of Florence and Venice.</p>
-                </div>
-            </div>
-
-            <div class="item">
-                <img src="http://html.crunchpress.com/big-salon/images/banner/banner-img-02.jpg">
-                <div class="carousel-caption">
-                    <h3>Chania</h3>
-                    <p>The atmosphere in Chania has a touch of Florence and Venice.</p>
-                </div>
-            </div>
-
-            <div class="item">
-                <img src="http://html.crunchpress.com/big-salon/images/banner/banner-img-03.jpg">
-                <div class="carousel-caption">
-                    <h3>Flowers</h3>
-                    <p>Beatiful flowers in Kolymbari, Crete.</p>
-                </div>
-            </div>
-
-            <div class="item">
-                <img src="http://html.crunchpress.com/big-salon/images/banner/banner-img-02.jpg">
-                <div class="carousel-caption">
-                    <h3>Flowers</h3>
-                    <p>Beatiful flowers in Kolymbari, Crete.</p>
-                </div>
-            </div>
-
-        </div>
-
-        <!-- Left and right controls -->
-        <a class="left carousel-control" href="#myCarousel" role="button">
-            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="right carousel-control" href="#myCarousel" role="button">
-            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
-    </div>
-
     <div class="container">
     <section>
         <div class="row">
