281d3d75c0d0541e3d2a59f4c032952853cdf46b
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-01-04 15:56:21 +0800

    footer less

--- resources/views/partial/footer.blade.php
+++ resources/views/partial/footer.blade.php
@@ -1,6 +1,6 @@
 <div class="col-md-12 f-footer-area">
 
-    <div class="row ">
+    <div class="row footer-row">
         <div class="col-xs-4 col-sm-4 col-md-4 menu">
             <img class="logo-jpg" src="img/logo-1.png" style="width: 241.4px; height: 150.6px;">
             <br>
@@ -19,7 +19,7 @@
         </div>
 
         <div class="col-xs-4 col-sm-4 col-md-4 enu">
-            <h3 class="widget-title heading-title" style="color:white;">Instagram posts</h3>
+            <h3 class="widget-title heading-title">Instagram posts</h3>
             <div class="line line-30"></div>
 
             <blockquote class="instagram-media" data-instgrm-version="7" style=" background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:658px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width:calc(100% - 2px);"><div style="padding:8px;"> <div style=" background:#F8F8F8; line-height:0; margin-top:40px; padding:50.0% 0; text-align:center; width:100%;"> <div style=" background:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACwAAAAsCAMAAAApWqozAAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAAMUExURczMzPf399fX1+bm5mzY9AMAAADiSURBVDjLvZXbEsMgCES5/P8/t9FuRVCRmU73JWlzosgSIIZURCjo/ad+EQJJB4Hv8BFt+IDpQoCx1wjOSBFhh2XssxEIYn3ulI/6MNReE07UIWJEv8UEOWDS88LY97kqyTliJKKtuYBbruAyVh5wOHiXmpi5we58Ek028czwyuQdLKPG1Bkb4NnM+VeAnfHqn1k4+GPT6uGQcvu2h2OVuIf/gWUFyy8OWEpdyZSa3aVCqpVoVvzZZ2VTnn2wU8qzVjDDetO90GSy9mVLqtgYSy231MxrY6I2gGqjrTY0L8fxCxfCBbhWrsYYAAAAAElFTkSuQmCC); display:block; height:44px; margin:0 auto -44px; position:relative; top:-22px; width:44px;"></div></div><p style=" color:#c9c8cd; font-family:Arial,sans-serif; font-size:14px; line-height:17px; margin-bottom:0; margin-top:8px; overflow:hidden; padding:8px 0 7px; text-align:center; text-overflow:ellipsis; white-space:nowrap;"><a href="https://www.instagram.com/p/BK4W_zfA-rt/" style=" color:#c9c8cd; font-family:Arial,sans-serif; font-size:14px; font-style:normal; font-weight:normal; line-height:17px; text-decoration:none;" target="_blank">A photo posted by Bella Make Up Salon Official (@isabellabellasmakeup)</a> on <time style=" font-family:Arial,sans-serif; font-size:14px; line-height:17px;" datetime="2016-09-28T01:08:36+00:00">Sep 27, 2016 at 6:08pm PDT</time></p></div></blockquote> <script async defer src="//platform.instagram.com/en_US/embeds.js"></script>
@@ -28,7 +28,7 @@
         </div>
 
         <div class="col-xs-4 col-sm-4 col-md-4 newsletter">
-            <h3 class="widget-title heading-title" style="color: white;">
+            <h3 class="widget-title heading-title">
 
                         Мэдээлэл авах                   </h3>
             <div class="line line-30"></div>
@@ -53,31 +53,24 @@
                 </div>
             </div>
             @endif
-            <div class="footer-bot-right-area">
-                <h3 class="widget-title heading-title" style="color: white;">
+            <div class="footer-right">
+                <h3 class="widget-title heading-title">
                         Сошиал                  </h3>
                 <div class="line line-30"></div>
                 <ul>
-                    <li style=" list-style: none; list-style-position: initial;">
+                    <li>
                         <div class="social-icons">
-                            <ul style="
-    padding-left: 0px;
-">
-                                <li class="icon-facebook fa-lg" style="border: 1px solid #f8f8f8;width: 12%;
-    text-align: center;"><a href="https://www.facebook.com/pg/Make-up-artist-Bella-451092605001683/videos/?ref=page_internal" target="_blank" title="Become our fan"><i class="fa fa-facebook"></i></a>
+                            <ul>
+                                <li class="icon-facebook fa-lg"><a href="https://www.facebook.com/pg/Make-up-artist-Bella-451092605001683/videos/?ref=page_internal" target="_blank" title="Become our fan"><i class="fa fa-facebook"></i></a>
                                 </li>
 
-                                <li class="icon-twitter fa-lg" style="border: 1px solid #f8f8f8;width: 12%;
-    text-align: center;"><a href="http://twitter.com/#" target="_blank" title="Follow us"><i class="fa fa-twitter"></i></a>
+                                <li class="icon-twitter fa-lg"><a href="http://twitter.com/#" target="_blank" title="Follow us"><i class="fa fa-twitter"></i></a>
                                 </li>
 
-                                <li class="icon-google fa-lg" style="border: 1px solid #f8f8f8;width: 12%;
-    text-align: center;"><a href="https://plus.google.com/u/0/#" target="_blank" title="Get updates"><i class="fa fa-google-plus"></i></a>
+                                <li class="icon-google fa-lg"><a href="https://plus.google.com/u/0/#" target="_blank" title="Get updates"><i class="fa fa-google-plus"></i></a>
                                 </li>
 
-                                
-                                <li class="icon-instagram fa-lg" style="border: 1px solid #f8f8f8;width: 12%;
-    text-align: center;"><a href="https://www.instagram.com/isabellabellasmakeup/" target="_blank" title="Follow us"><i class="fa fa-instagram"></i></a>
+                                <li class="icon-instagram fa-lg"><a href="https://www.instagram.com/isabellabellasmakeup/" target="_blank" title="Follow us"><i class="fa fa-instagram"></i></a>
                                 </li>                            
                             </ul>
                         </div>
@@ -97,23 +90,19 @@
             <div class="payment col-sm-4">
                 <ul>
                     <li>
-                        <a href="#"><img src="http://glory.demo1.wpdance.com/supermarket2/wp-content/themes/wp_glory/images/media/paypal.jpg">
+                        <a href="#"><img src="img/paypal/paypal.jpg">
                         </a>
                     </li>
                     <li>
-                        <a href="#"><img src="http://glory.demo1.wpdance.com/supermarket2/wp-content/themes/wp_glory/images/media/mastercard.jpg">
+                        <a href="#"><img src="img/paypal/mastercard.jpg">
                         </a>
                     </li>
                     <li>
-                        <a href="#"><img src="http://glory.demo1.wpdance.com/supermarket2/wp-content/themes/wp_glory/images/media/american.jpg">
+                        <a href="#"><img src="img/paypal/american.jpg">
                         </a>
                     </li>
                     <li>
-                        <a href="#"><img src="http://glory.demo1.wpdance.com/supermarket2/wp-content/themes/wp_glory/images/media/visa.jpg">
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#"><img src="http://glory.demo1.wpdance.com/supermarket2/wp-content/themes/wp_glory/images/media/dhl.jpg">
+                        <a href="#"><img src="img/paypal/visa.jpg">
                         </a>
                     </li>
                 </ul>
