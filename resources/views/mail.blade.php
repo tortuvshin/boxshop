b3d2834eb9e5e775627d6eada1d8cb7d24b2a731
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-02-12 22:19:56 +0800

    gulp remove, mail blade updated

--- resources/views/mail.blade.php
+++ resources/views/mail.blade.php
@@ -8,23 +8,23 @@
 
  <div class="row">
             <div class="col-md-12">
-                <h1>Contact Me</h1>
+                <h1>Холбоо барих</h1>
                 <hr>
                 <form action="{{ url('contact') }}" method="POST">
                     {{ csrf_field() }}
                     <div class="form-group">
-                        <label name="email">Email:</label>
+                        <label name="email">Мэйл хаяг:</label>
                         <input id="email" name="email" class="form-control">
                     </div>
 
                     <div class="form-group">
-                        <label name="subject">Subject:</label>
+                        <label name="subject">Гарчиг:</label>
                         <input id="subject" name="subject" class="form-control">
                     </div>
 
                     <div class="form-group">
-                        <label name="message">Message:</label>
-                        <textarea id="message" name="message" class="form-control">Type your message here...</textarea>
+                        <label name="message">Зурвас:</label>
+                        <textarea id="message" name="message" class="form-control">Зурвас бичнэ үү...</textarea>
                     </div>
 
                     <input type="submit" value="Send Message" class="btn btn-success">
