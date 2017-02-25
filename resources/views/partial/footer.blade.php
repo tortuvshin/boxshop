dcde3755254e87afdd7350c1e1f7df77ed491833
DESKTOP-V2RFREH\Baagii <batgerel742@gmail.com>
2017-02-26 00:03:20 +0800

    Glyphicon

--- resources/views/partial/footer.blade.php
+++ resources/views/partial/footer.blade.php
@@ -2,8 +2,7 @@
 
     <div class="row footer-row">
         <div class="col-xs-4 col-sm-4 col-md-4">
-            <img class="logo-jpg" src="img/logo-1.png">
-            
+            <img class="logo-jpg" src="img/box-shop/logo.png">
         </div>
 
         <div class="col-xs-4 col-sm-4 col-md-4 menu">
