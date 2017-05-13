dcf8776ed047049c2ba6f70e69d3a974ca9fd4e8
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-05-13 21:42:40 +0800

    dashboard route controller views updated

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,28 +1,37 @@
-@extends('layouts/master')
-
-@section('title')@parent- {{ trans('user.dashboard') }} @stop
-
-@section('page_class') 'user-dashboard' @stop
-
-@section('content')
-	@parent
-	@section('panel_left_content')
-		@include('user.partial.menu_dashboard')
-	@stop
-	@section('center_content')
-
-		<div class="page-header">
-			<h5>{{ trans('user.dashboard') }}</h5>
-		</div>
-	@stop
-@endsection
-{{-- Pie de pagina --}}
-@section('footer')
-	@parent
+@extends('layouts.dashboard')
+@section('title')@parent- {{ trans('company.store_config') }} @stop
+@section('panel_left_content')
+@parent
 @stop
-{{-- Javascript --}}
-@section('scripts')
+@section('center_content')
+    
+    <h1>Удирдлагын хэсэг</h1>
 
-	@parent
+    <div class="row placeholders">
+        <div class="col-xs-6 col-sm-3 placeholder">
+            <img data-src="holder.js/200x200/auto/sky" class="img-responsive" alt="Generic placeholder thumbnail">
+            <h4>Жишээ</h4>
+            <span class="text-muted">Жишээ</span>
+        </div>
+        <div class="col-xs-6 col-sm-3 placeholder">
+            <img data-src="holder.js/200x200/auto/vine" class="img-responsive" alt="Generic placeholder thumbnail">
+            <h4>Жишээ</h4>
+            <span class="text-muted">Жишээ</span>
+        </div>
+        <div class="col-xs-6 col-sm-3 placeholder">
+            <img data-src="holder.js/200x200/auto/sky" class="img-responsive" alt="Generic placeholder thumbnail">
+            <h4>Жишээ</h4>
+            <span class="text-muted">Жишээ</span>
+        </div>
+        <div class="col-xs-6 col-sm-3 placeholder">
+            <img data-src="holder.js/200x200/auto/vine" class="img-responsive" alt="Generic placeholder thumbnail">
+            <h4>Жишээ</h4>
+            <span class="text-muted">Жишээ</span>
+        </div>
+    </div>
+   
 @stop
 
+@section('script')
+    @parent
+@stop
